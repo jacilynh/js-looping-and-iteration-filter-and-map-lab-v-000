e665c3e25c4fed3cacaee5c47b62f26893851525
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-22 00:44:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,8 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 }
 
 // * `exactMatch()` - This function takes an array of `driver` objects as the first argument and a JavaScript object that specifies an attribute and corresponding value. For example, `exactMatch(drivers, { revenue: 3000 })` will return all drivers whose `revenue` attribute equals `3000`, and `exactMatch(drivers, { name: 'Bob' })` will return all drivers whose `name` attribute equals `Bob`.
+function exactMatch(driver, {}) {
+  
+}
 
 // * `exactMatchToList()` - This function works the same way as the `exactMatch()` function, except that instead of returning an array of `driver` objects, it returns an array of strings with each element in the array corresponding to the matching driver's name. For example, `exactMatchToList(drivers, { revenue: 2000 })` will return `['Sammy']`, as `Sammy` is the name of the matching `driver` object.
