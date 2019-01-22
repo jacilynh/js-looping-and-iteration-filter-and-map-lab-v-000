ab96e609153e8cf65647abacc9cc89587a4c3b31
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-22 00:38:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function driversWithRevenueOver(driver, revenue) {
 }
 
 // * `driverNamesWithRevenueOver()` - This function works the same way as `driversWithRevenueOver()`. The difference is that it returns an array of strings representing the name of each driver who has a revenue greater than the specified amount. For example, `driverNamesWithRevenueOver(drivers, 2000)` will return `['Sammy']`, as `Sammy` is the name of the matching `driver` object with a revenue greater than `2000`.
-function driverNamesWithRevenueOver()
+function driverNamesWithRevenueOver(drivers, revenue) {
+  array = drivers.filter(d => d.revenue > revenue)
+  return array.map(d => d.name)
+}
 
 // * `exactMatch()` - This function takes an array of `driver` objects as the first argument and a JavaScript object that specifies an attribute and corresponding value. For example, `exactMatch(drivers, { revenue: 3000 })` will return all drivers whose `revenue` attribute equals `3000`, and `exactMatch(drivers, { name: 'Bob' })` will return all drivers whose `name` attribute equals `Bob`.
 
