d9bc8c68ed435a0e8924d5334bac87c253e8deb8
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-22 00:32:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // * `driversWithRevenueOver`- This function takes an array of `driver` JavaScript objects as the first argument and a number called `revenue` as the second argument. It returns an array of `driver` objects that have a `revenue` attribute that's greater than the passed-in `revenue` argument.
 function driversWithRevenueOver(driver, revenue) {
-  return drivers.filter(d => d.revenue > revenue)
+  return drivers.filter(driver => driver.revenue > revenue)
 }
 
 // * `driverNamesWithRevenueOver()` - This function works the same way as `driversWithRevenueOver()`. The difference is that it returns an array of strings representing the name of each driver who has a revenue greater than the specified amount. For example, `driverNamesWithRevenueOver(drivers, 2000)` will return `['Sammy']`, as `Sammy` is the name of the matching `driver` object with a revenue greater than `2000`.
